82fe1d2f19dc27de603fe3d04e63cda04beae2eb
NIKITA SHORYCHEV <87395156+SAMAKAKI@users.noreply.github.com>
2022-12-07 10:12:09 +0100

    Update settings.py

--- samakaki/samakaki/settings.py
+++ samakaki/samakaki/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u5$&vs&8_-zh81*jjppqfi7f+0x-m8!)r-hl8%@cl+rpwp$3yg'
+SECRET_KEY = 'your key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
